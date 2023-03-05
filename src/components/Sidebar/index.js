18fc1ed7191bc04578ff08a22caa02e185e9d8b5
Abed-Nassar <abdelsalam_nassar@hotmail.com>
2023-03-04 22:48:10 -0400

    temp

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,6 +1,5 @@
 import "./index.scss";
 import LogoS from "../../assets/images/logo-oold.png"; // changed logo-s to logo-old
-import codechefLogo from "../../assets/images/codechef.png";
 import LogoSubtitle from "../../assets/images/Abed_logo_white.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
--- src/components/Soundbar/index.js
+++ src/components/Soundbar/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useRef, useState } from "react";
 import styled, { keyframes } from "styled-components";
 
 import music from "../../assets/sounds/song.mp3";
