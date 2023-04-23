163f5099b7bca4aba12041b0d45a1b8d85096d71
Abed-Nassar <abdelsalam_nassar@hotmail.com>
2023-04-22 22:08:40 -0400

    datacenterspec

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -63,7 +63,7 @@ const Home = () => {
             />
           </h1>
           <h2>
-             Database Admin / Embedded System Developer / AI/ML Developer
+          Data Center Specialist / Database Admin  / AI/ML Developer
           </h2>
           <Link to="/contact" className="flat-button">
             CONTACT ME
